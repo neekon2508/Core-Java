f0cea8852dc93fa7a2f456e5566fda38c3a30693
Neekon2508 <nghia250803@gmail.com>
2026-05-01 04:10:31 +0700

    Core Java 1: update chap 12 multi thread

--- Concurrency/Test/test1/App.java
+++ Concurrency/Test/test1/App.java
@@ -1,51 +1,23 @@
 package Concurrency.Test.test1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class App {
-    private static final Object LOCK = new Object();
 
-    public static void main(String[] args) throws InterruptedException {
-        // 1. Tạo luồng gây ra trạng thái BLOCKED
-        Thread thread1 = new Thread(() -> {
-            synchronized (LOCK) {
-                System.out.println("Thread 1: Đã giữ LOCK và đang ngủ (không nhả khóa)...");
-                try {
-                    Thread.sleep(30000); // Ngủ rất lâu
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, "Ngu-Giu-Khoa-Thread");
-
-        Thread thread2 = new Thread(() -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-            }
-            System.out.println("Thread 2: Đang cố vào synchronized block nhưng bị chặn...");
-            synchronized (LOCK) {
-                try {
-                    System.out.println("Thread 2: Cuối cùng cũng vào được!");
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, "Bi-Blocked-Thread");
-
-        // 2. Tạo luồng ở trạng thái WAITING
-        Thread thread3 = new Thread(() -> {
-            synchronized (LOCK) {
-                try {
-                    System.out.println("Thread 3: Đang gọi wait() (nhả khóa để đợi)...");
-                    LOCK.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, "Dang-Wait-Thread");
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(8);
+        ExecutorCompletionService<Integer> service = new ExecutorCompletionService<>(executor);
+        List<Callable<Integer>> tasks = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            int a = i;
+            Callable<Integer> t = () -> a * 10;
+            tasks.add(t);
+        }
+        for (Callable<Integer> task : tasks)
+            service.submit(task);
+        for (int i = 0; i < tasks.size(); i++)
+            System.out.println(service.take().get());
     }
 }
--- Concurrency/Test/test1/README.md
+++ Concurrency/Test/test1/README.md
@@ -16,3 +16,19 @@ Meanwhile, the compiled output files will be generated in the `bin` folder by de
 ## Dependency Management
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+
+# Summary
+
+A [Multi-threading] --> B(Concurrency - Quản lý việc)
+A --> C(Parallelism - Tăng tốc độ)
+
+    B --> B1[Synchronized - Khóa cứng]
+    B --> B2[ReentrantLock - Khóa linh hoạt]
+    B --> B3[Atomic/CAS - Không khóa]
+
+    C --> C1[Fork-Join Pool - Chia để trị]
+    C --> C2[Parallel Stream - Tận dụng 16 luồng]
+
+Synchronous (gọi hàm, đợi kết quả mới chạy dòng code tiếp theo) / Asynchronous (gọi hàm, đăng ký callback và chạy ngay dòng code tiếp theo): Nói về kết quả
+
+Blocking (Luồng wait chờ kết quả) / Non-blocking (Luồng trả về ngay Promise/Future): Nói về trạng thái của luồng. Luồng có được phép làm việc khác trong khi đợi kết quả hay không?
